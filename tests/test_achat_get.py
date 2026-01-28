7395c1fb30dc4b2ad474b16afb642d11b7eb9e91
WIAM02 <wiamhaoudi16@gmail.com>
2026-01-28 12:39:44 +0100

    my

--- tests/test_achat_get.py
+++ tests/test_achat_get.py
@@ -39,5 +39,8 @@ def test_get_achat_with_mission_id_returns_200():
     print("BODY:", response.text)
     print("BODY:", response.text)
     print("BOD11Y:", response.text)
+    print("BOD11Y:", response.text)
+    print("BOD11Y:", response.text)
+    print("BOD11Y:", response.text)
 
     assert response.status_code == 400
